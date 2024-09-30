007c9d4912be778ba48011866d935f844a213b41
Im Chaeeun <dev.chaeng@gmail.com>
2024-09-30 21:01:35 +0900

    리소스 로드 경로 수정

--- GameCoding/Game.cpp
+++ GameCoding/Game.cpp
@@ -35,7 +35,7 @@ void Game::Init(HWND hwnd)
 	GET_SINGLE(TimeManager)->Init();
 	GET_SINGLE(InputManager)->Init(hwnd);
 	GET_SINGLE(SceneManager)->Init();
-	GET_SINGLE(ResourceManager)->Init(hwnd, fs::path(L"C:\\Rookiss\\Inflearn_WinAPI\\Resources"));
+	GET_SINGLE(ResourceManager)->Init(hwnd, fs::path(L"C:\\Zelda_GameCoding\\Resources"));
 	GET_SINGLE(SoundManager)->Init(hwnd);
 
 	GET_SINGLE(SceneManager)->ChangeScene(SceneType::DevScene);
